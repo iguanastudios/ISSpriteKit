6b80c5023b9213436b0503f338c756a1e37efd58
Luis Flores <me@luisflores.mx>
2014-03-17 23:22:24 +0000

    Initial Commit

---
+++ ISSpriteKit/ISViewController.h
@@ -0,0 +1,14 @@
+//
+//  ISViewController.h
+//  ISSpriteKit
+//
+
+//  Copyright (c) 2014 Iguana Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ISViewController : UIViewController
+
+@end
---
+++ ISSpriteKitTests/ISSpriteKitTests.m
@@ -0,0 +1,34 @@
+//
+//  ISSpriteKitTests.m
+//  ISSpriteKitTests
+//
+//  Created by Luis Flores on 3/17/14.
+//  Copyright (c) 2014 Iguana Studios. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ISSpriteKitTests : XCTestCase
+
+@end
+
+@implementation ISSpriteKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
